f346e571c9ef37838b4e5869191ee585d8c19620
Terry Park <58895710+park-jsdev@users.noreply.github.com>
2023-06-02 08:30:39 -0700

    Add python script timer

---
+++ python/script_timer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  2 08:12:55 2023
+
+@author: JP
+From: https://stackoverflow.com/questions/5622976/how-do-you-calculate-program-run-time-in-python
+"""
+from datetime import datetime
+start=datetime.now()
+
+# Statements
+
+print(datetime.now()-start)
