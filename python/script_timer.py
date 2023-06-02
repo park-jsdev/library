d95037ba4c417134d18f6671f466596a7fb75e7f
Terry Park <58895710+park-jsdev@users.noreply.github.com>
2023-06-02 14:46:54 -0700

    update script_timer

--- python/script_timer.py
+++ python/script_timer.py
@@ -1,13 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  2 08:12:55 2023
+import time
 
-@author: JP
-From: https://stackoverflow.com/questions/5622976/how-do-you-calculate-program-run-time-in-python
-"""
-from datetime import datetime
-start=datetime.now()
+# Timer
+start_time = time.time()
 
-# Statements
+print('Runtime: ', time.time()-start_time, 'seconds.')
 
-print(datetime.now()-start)
+# To print estimated completion time within script
+def est_completion_time(start_time, OBSERVED_COMPLETION_TIME):
+    current_time = time.time()-start_time
+    est_completion_rate = current_time/OBSERVED_COMPLETION_TIME
+
+    if est_completion_time <= 99.9:
+        res = str("%.2f" % (current_time/OBSERVED_COMPLETION_TIME)) + "%"
+    else:
+        res = "99.99"
+    return res
