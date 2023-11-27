4125e4d4cc94cd6cfaddc5f3c599a8421e5fdb58
Terry Junsoo Park <58895710+park-jsdev@users.noreply.github.com>
2023-11-27 06:02:08 -0800

    dsa trees and binary search

---
+++ java/patterns/binary_search/LC704_binary_search.java
@@ -0,0 +1,22 @@
+// Binary Search
+// Time: O(logn)
+// Space: O(1)
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+
+        while (l <= r){
+            int mid = l + (r-l)/2;
+            if (nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target){
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
