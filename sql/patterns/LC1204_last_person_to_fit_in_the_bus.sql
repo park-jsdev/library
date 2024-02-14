d1ea0da7b1b9080816a534b0217f8ee94293adca
Terry Junsoo Park <58895710+park-jsdev@users.noreply.github.com>
2024-02-14 05:36:16 -0800

    python and sql

---
+++ sql/patterns/LC1204_last_person_to_fit_in_the_bus.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+select person_name
+from (select person_name, sum(weight) over (order by turn) as sum_weight from queue
+    order by sum_weight desc)
+where sum_weight <= 1000
+    and rownum = 1;
