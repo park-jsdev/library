f44b449257105d9737be3186a546d233601c6965
Terry Junsoo Park <58895710+park-jsdev@users.noreply.github.com>
2024-02-27 06:40:19 -0800

    dsa trees

---
+++ java/patterns/trees/LC222_count_complete_tree_nodes.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        
+        int leftHeight = leftHeight(root);
+        int rightHeight = rightHeight(root);
+        
+        // If left and right heights are equal, the tree is perfect and fully filled
+        if (leftHeight == rightHeight) {
+            return (int)Math.pow(2, leftHeight) - 1;
+        } else {
+            // Recursively count nodes in left and right subtrees
+            return 1 + countNodes(root.left) + countNodes(root.right);
+        }
+    }
+    
+    private int leftHeight(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            height++;
+            node = node.left;
+        }
+        return height;
+    }
+    
+    private int rightHeight(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            height++;
+            node = node.right;
+        }
+        return height;
+    }
+}
