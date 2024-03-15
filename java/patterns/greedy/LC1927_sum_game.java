fb6f5ae4bb0c45a570a6b24cb8adcaf255c311c5
Terry Junsoo Park <58895710+park-jsdev@users.noreply.github.com>
2024-03-15 07:33:23 -0700

    dsa greedy

---
+++ java/patterns/greedy/LC1927_sum_game.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int diff = 0;
+        int left = 0;
+        int right = 0;
+        for (int i=0;i<num.length()/2;i++){
+            if (num.charAt(i) == '?'){
+                left++;
+            } else {
+                diff += num.charAt(i) - '0';
+            }
+        }
+        for (int i=num.length()/2;i<num.length();i++){
+            if (num.charAt(i) == '?'){
+                right++;
+            } else {
+                diff -= num.charAt(i) - '0';
+            }
+        }
+        // Situation 2
+        if ((right + left) % 2 == 1) return true;
+        // Situation 1 and 3
+        if (left == right){
+            return diff != 0;
+        }
+        int round = (left - right) / 2;
+        // Situation a
+        if (diff * round >= 0) return true;
+        return Math.abs(diff) != Math.abs(round * 9);
+    }
+}
