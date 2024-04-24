88e61130c42ec42f79f9f10fc70df47406476831
Terry Junsoo Park <58895710+park-jsdev@users.noreply.github.com>
2024-04-24 06:41:31 -0700

    group by json

---
+++ javascript/patterns/LC2631_group_by.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    return this.reduce((grouped, item) => {
+        const key = fn(item);
+
+        if (!grouped[key]){
+            grouped[key] = [];
+        }
+        grouped[key].push(item);
+
+        return grouped;
+    }, {});
+};
