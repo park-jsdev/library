dcb7db02fcc218371831609a58e883ca1af91611
Terry Junsoo Park <58895710+park-jsdev@users.noreply.github.com>
2024-07-14 16:30:51 -0700

    mern api jenkins pipeline and k8s

--- webdev/mern-k8s/mern-app/backend/models/Contact.js
+++ webdev/mern-k8s/mern-app/backend/models/Contact.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-const formSchema = mongoose.Schema({
+const contactSchema = mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String },
   phone: { type: String, required: true },
   socialMedia: { type: String },
-  group: { type: String },
+  group: { type: String, required: true },
   country: { type: String },
   carrier: { type: String },
   lineType: { type: String },
 }, { timestamps: true });
 
-const Form = mongoose.model('Form', formSchema);
-module.exports = Form;
+const Contact = mongoose.model('Contact', contactSchema);
+module.exports = Contact;
--- webdev/mern/mern-app/backend/routes/formRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createForm, getFormById, getAllForms, deleteFormById, updateFormById } = require('../controllers/formController');
-
-router.route('/').post(createForm).get(getAllForms);
-router.route('/:id').get(getFormById).delete(deleteFormById).put(updateFormById);
-
-module.exports = router;
