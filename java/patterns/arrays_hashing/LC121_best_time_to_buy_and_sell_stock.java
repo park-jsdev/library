599dd53ff477bbb796387bfed1f6063907fd2d84
Terry Junsoo Park <58895710+park-jsdev@users.noreply.github.com>
2024-07-24 10:35:11 -0700

    dsa kadanes

---
+++ java/patterns/arrays_hashing/LC121_best_time_to_buy_and_sell_stock.java
@@ -0,0 +1,14 @@
+// Kadanes Algorithm, Max subarray
+// Time: O(n)
+// Space: O(1)
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxCur = 0, maxSoFar = 0;
+        for (int i=1;i<prices.length;i++){
+            maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+            maxSoFar = Math.max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
+    }
+}
